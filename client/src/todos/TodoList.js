b14926e5a844f7d123be44a44eba0c805d41a2ad
kdoh52 <kdoh52@gmail.com>
2021-02-24 16:16:54 -0800

    working form, delete

--- client/src/todos/TodoList.js
+++ client/src/todos/TodoList.js
@@ -3,11 +3,25 @@ import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 
-const TodoList = ({ todos = [] }) => (
+// connect component to redux
+import { connect } from "react-redux";
+import { removeTodo } from "./actions";
+
+const TodoList = ({ todos = [], onRemovePressed }) => (
     <div className="list-wrapper">
         <NewTodoForm />
-        {todos.map(todo => <TodoListItem todo={todo} />)}
+        {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed}/>)}
     </div>
 );
 
-export default TodoList;
+
+// connecting todolist component to store
+const mapStateToProps = state => ({
+    todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => ({
+    onRemovePressed: text => dispatch(removeTodo(text))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
